0137f5f9af509607acc0fcfae7bea13358aafcf4
AyushR1 <ayushrathore12501@gmail.com>
2023-04-20 01:43:37 +0530

    Implement Login api with jwt
    Signed-off-by: AyushR1 <ayushrathore12501@gmail.com>

--- users/serializers.py
+++ users/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth import authenticate
 
 class RegisterSerialiser(serializers.Serializer):
     username = serializers.CharField(max_length = 255, min_length = 3)
@@ -13,3 +14,23 @@ class RegisterSerialiser(serializers.Serializer):
     
     def create(self, validated_data):
         return User.objects.create_user(**validated_data)
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField(max_length = 255, min_length = 3)  
+    password = serializers.CharField(min_length = 8, write_only = True)
+
+    def validate(self, data):
+        if not User.objects.filter(username=data['username']).exists():
+            raise serializers.ValidationError({'username': ('Username is not found')})
+        return data
+    
+    def get_jwt_token(self, validated_data):
+        user = authenticate(username=validated_data['username'], password=validated_data['password'])
+        if not user:
+            return {'password': ('Invalid credentials')}
+        
+        refresh = RefreshToken.for_user(user)
+        return {
+            'refresh': str(refresh),
+            'access': str(refresh.access_token),
+        }
--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .serializers import RegisterSerialiser
+from .serializers import RegisterSerialiser, LoginSerializer
 
 class RegisterView(APIView):
     serializer_class = RegisterSerialiser
@@ -13,5 +13,18 @@ class RegisterView(APIView):
             return Response(serializer.errors, status = 400)
         except Exception as e:
             return Response({'error': str(e)}, status = 500)
+
+
+class LoginView(APIView):
+    serializer_class = LoginSerializer
+    def post(self, request):
+        try:
+            serializer = self.serializer_class(data = request.data)
+            if serializer.is_valid():
+                return Response(serializer.get_jwt_token(serializer.validated_data), status = 200)
+            return Response(serializer.errors, status = 400)
+        except Exception as e:
+            return Response({'error': str(e)}, status = 500)
+
 
 # Create your views here.
