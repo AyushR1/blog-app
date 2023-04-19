1b76f1d3aaafb9e532e12b538ac78eb0e876e546
AyushR1 <ayushrathore12501@gmail.com>
2023-04-20 03:22:54 +0530

    Implement the get pactch and post api
    Signed-off-by: AyushR1 <ayushrathore12501@gmail.com>

--- posts/views.py
+++ posts/views.py
@@ -4,6 +4,8 @@ from rest_framework import status
 from .serializers import BlogSerializer
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
+from .models import Blog
+
 
 class PostView(APIView):
     permission_classes = [IsAuthenticated]
@@ -22,4 +24,34 @@ class PostView(APIView):
         except Exception as e:
             return Response({'error': str(e)}, status = 500)
 
+    def get(self,request):
+        try:
+            blogs=Blog.objects.filter(user=request.user)
+            serializer=self.serializer_class(blogs,many=True)
+            return Response(serializer.data,status=200)
+        except Exception as e:
+            return Response({'error': str(e)}, status = 500) 
+    
+    def patch(self, request):
+        try:
+            data=request.data
+            blog=Blog.objects.filter(uid = data.get('uid'))
+            
+            if not blog.exists():
+                return Response({'error': 'Blog does not exist'}, status = 400)
+
+            if request.user.id != blog[0].user.id:
+                return Response({'error': 'You are not authorized to update this blog'}, status = 400)
+            
+            serializer = self.serializer_class(blog[0],data = data,partial=True)
+            
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status = 201)
+            return Response(serializer.errors, status = 400)
+        except Exception as e:
+            return Response({'errore': str(e)}, status = 500)
+
     # Create your views here.
+
+
