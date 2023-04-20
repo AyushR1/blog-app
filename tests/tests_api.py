3c798a7846962b970a1eb0f5ea55de6e18ed19c7
AyushR1 <ayushrathore12501@gmail.com>
2023-04-20 10:28:55 +0530

    Add login test
    Signed-off-by: AyushR1 <ayushrathore12501@gmail.com>

--- tests/tests_api.py
+++ tests/tests_api.py
@@ -11,3 +11,16 @@ class UserRegistrationTestCase(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(User.objects.count(), 1)
         self.assertEqual(User.objects.get().username, 'testuser')
+
+
+class UserLoginTestCase(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+    
+    def test_login(self):
+        data = {'username': 'testuser', 'password': 'testpassword'}
+        response = self.client.post('/api/login/', data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn('refresh', response.data)
+        self.assertIn('access', response.data)
+
