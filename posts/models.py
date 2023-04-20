538c333f27e968a5fe3b6e9d8eec9be79922dd73
AyushR1 <ayushrathore12501@gmail.com>
2023-04-20 11:56:27 +0530

    Fix issues with uid
    Signed-off-by: AyushR1 <ayushrathore12501@gmail.com>

--- posts/models.py
+++ posts/models.py
@@ -3,16 +3,16 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class BaseModel(models.Model):
-    uid = models.AutoField(primary_key=True)
-    created_at = models.DateTimeField(auto_now_add = True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
     class Meta:
         abstract = True
 
 class Blog(BaseModel):
-    user = models.ForeignKey(User, on_delete = models.CASCADE, related_name='blogs')
-    title = models.CharField(max_length = 255)
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE, related_name='blogs')
+    title = models.CharField(max_length=255)
     description = models.TextField()
-    image = models.ImageField(upload_to = 'blogsimg',default='blogsimg/default.png')
+    image = models.ImageField(upload_to='blogsimg', default='blogsimg/default.png')
 
     def __str__(self):
         return self.title
