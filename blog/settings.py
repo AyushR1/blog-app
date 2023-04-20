15c60a05bf2519decbd75219b861b36ee933acf2
AyushR1 <ayushrathore12501@gmail.com>
2023-04-20 13:28:31 +0530

    Setup Docker
    Signed-off-by: AyushR1 <ayushrathore12501@gmail.com>

--- blog/settings.py
+++ blog/settings.py
@@ -77,13 +77,13 @@ WSGI_APPLICATION = "blog.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'blog',
-        'USER': 'ayushr1',
-        'PASSWORD': '12345678',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "db",  # set in docker-compose.yml
+        "PORT": 5432,  # default postgres port
     }
 }
 
