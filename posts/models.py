0db32701d70f4ab9e593bf384fcd4fbb83658c8e
AyushR1 <ayushrathore12501@gmail.com>
2023-04-20 09:47:49 +0530

    Switch to PostGRESQL
    Signed-off-by: AyushR1 <ayushrathore12501@gmail.com>

--- posts/models.py
+++ posts/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-import uuid
 # Create your models here.
 
 class BaseModel(models.Model):
@@ -13,7 +12,7 @@ class Blog(BaseModel):
     user = models.ForeignKey(User, on_delete = models.CASCADE, related_name='blogs')
     title = models.CharField(max_length = 255)
     description = models.TextField()
-    image = models.ImageField(upload_to = 'blogsimg')
+    image = models.ImageField(upload_to = 'blogsimg',default='blogsimg/default.png')
 
     def __str__(self):
         return self.title
